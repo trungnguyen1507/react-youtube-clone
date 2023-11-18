485b96d21a90de0dae2116a47a8a3f621ea0a53d
trungndse150553 <trungndse150553@fpt.edu.vn>
2023-11-18 22:33:46 +0700

    edit path 3

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -11,7 +11,7 @@ const VideoCard = ({
 }) => {
     return (
         <Card sx={{ width: { xs: "100%", sm: "358px", md: "320px" }, boxShadow: "none", borderRadius: 0 }}>
-            <Link to={videoId ? `react-youtube-clone/video/${videoId}` : demoVideoUrl}>
+            <Link to={videoId ? `/react-youtube-clone/video/${videoId}` : demoVideoUrl}>
                 <CardMedia
                     image={snippet?.thumbnails?.high?.url}
                     alt={snippet?.title}
@@ -19,12 +19,12 @@ const VideoCard = ({
                 />
             </Link>
             <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
-                <Link to={videoId ? `react-youtube-clone/video/${videoId}` : demoVideoUrl}>
+                <Link to={videoId ? `/react-youtube-clone/video/${videoId}` : demoVideoUrl}>
                     <Typography variant="subtitle1" fontWeight="bold" color="#fff">
                         {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
                     </Typography>
                 </Link>
-                <Link to={snippet?.channelId ? `react-youtube-clone/channel/${snippet?.channelId}` : demoChannelUrl}>
+                <Link to={snippet?.channelId ? `/react-youtube-clone/channel/${snippet?.channelId}` : demoChannelUrl}>
                     <Typography variant="subtitle2" fontWeight="bold" color="gray">
                         {snippet?.channelTitle || demoChannelTitle}
                         <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
